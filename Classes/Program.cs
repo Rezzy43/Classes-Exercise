d79cd1a3630baef6484c7c4e62c505eedd3e6f3a
Michael Resendez <michael.resendez1@hotmail.com>
2024-11-04 12:55:10 -0600

    Completed Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Classes
+﻿public class Car
 {
-    public class Program
+    // Properties
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // I'm creating a new instance of the Car class
+        Car myCar = new Car();
+        
+        // Setting values in the properties
+        myCar.Make = "Toyota";
+        myCar.Model = "Supra";
+        myCar.Year = 2025;
+        
+        // Printing the values of each property to the console
+        Console.WriteLine($"Make: {myCar.Make}");
+        Console.WriteLine($"Model: {myCar.Model}");
+        Console.WriteLine($"Year: {myCar.Year}");
     }
 }
